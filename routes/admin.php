4589ada1c08b43fcb22e47cec42d28649f08bc35
marencoalf <alfredomarenco@boletea.com>
2021-09-08 21:09:35 -0500

    Backup

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ConfigurationController;
 use App\Http\Livewire\Slider;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('dashboard');
+});
+
+
+Route::prefix('configurations')->group(function () {
+    Route::get('index', [ConfigurationController::class, 'index']);
 });
