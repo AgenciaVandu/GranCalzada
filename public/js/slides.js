0c0b73c5f4880a6bd6ed87adecad796a07c806da
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-09-22 10:37:45 -0500

    se agrego contador a la seccion de grupo casitas en gran calzada

--- public/js/slides.js
+++ public/js/slides.js
@@ -60,3 +60,35 @@ window.addEventListener('load', function(){
 
 });
 
+  let cont1=document.getElementById('cont1'),
+      cont2=document.getElementById('cont2'),
+      cont3=document.getElementById('cont3')
+  let cant1=0,cant2=0,cant3=0
+      tiem=25
+
+  let tiempo1= setInterval(() => {
+        cont1.textContent=cant1+=1
+
+        if (cant1==19) {
+          clearInterval(tiempo1)
+        }
+
+      }, '${tiem}' );
+
+  let tiempo2= setInterval(() => {
+    cont2.textContent=cant2+=1
+
+        if (cant2==8438) {
+          clearInterval(tiempo2)
+        }
+
+      }, '${tiem}' );
+
+  let tiempo3= setInterval(() => {
+    cont3.textContent=cant3+=1
+
+        if (cant3==32908) {
+          clearInterval(tiempo3)
+        }
+
+      }, '${tiem}' );
