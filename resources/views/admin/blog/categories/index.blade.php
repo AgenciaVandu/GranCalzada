1937b23b9fc411278c197c07321dc171c8788457
unknown <alfredomarenco@boletea.com>
2021-09-22 08:11:56 -0500

    Agregamos boton para crear categorias

--- resources/views/admin/blog/categories/index.blade.php
+++ resources/views/admin/blog/categories/index.blade.php
@@ -8,6 +8,9 @@
     <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="flex justify-end items-center bg-gray-100 p-4">
+                    <x-link href="{{ route('categories.create') }}">Nueva categoria</x-link>
+                </div>
                 <div class="flex flex-col">
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
