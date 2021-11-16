6606079eb486b935fd171ebff5a38cc8075ec1c3
unknown <alfredomarenco@boletea.com>
2021-11-16 12:56:48 -0600

    Cambios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -401,7 +401,7 @@
                     </div>
                 </div>
                 <div class="col-12 pb-5">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
                             @isset($slider->resources)
                                 @foreach ($slider->resources as $resource)
@@ -416,11 +416,11 @@
                                 </div>
                             @endisset
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>
