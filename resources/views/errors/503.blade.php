d62add15fd06f1d7e970d38657740f3943bce9ec
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-12-01 16:14:52 -0600

    paguen

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,6 @@
 @extends('errors::illustrated-layout')
 
-@section('title', __('Service Unavailable'))
+@section('title', __('Servicio no disponible'))
 @section('code', '503')
 @section('message', __('Servicio no disponible'))
 @section('image')
